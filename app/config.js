a72a2081ea4df82e5f36c405b19e0831475c4289
offish <overutilization@gmail.com>
2019-02-21 11:01:39 +0100

    Create config.js

---
+++ app/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    bot: 
+    {
+        nickname:           "⚡ GenericBot 24/7 ⚡",
+        username:           "username", // Username on Steam, not nickname.
+        password:           "password", // Password
+        shared_secret:      "shared=",
+        identity_secret:    "identity="
+    },
+
+    ids: 
+    {
+        group:  103582791440562795,
+        bot:    12312312312312312,
+        owner:  76561198253325712
+    }
+}
