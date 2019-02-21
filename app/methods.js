e105fe2d07615af9c2d25d88c18094581fc5a90a
offish <overutilization@gmail.com>
2019-02-21 11:00:48 +0100

    Update methods.js

--- app/methods.js
+++ app/methods.js
@@ -1,9 +1,23 @@
-var t = modules.exports = {
+const config = require('./config')
 
- isTF2: function(item) {
-    return item.appid == 440;
-  },
-  
-  isCraftable : function(item) {
-  }
+
+
+var t = module.exports = {
+    isDonation: function(offer) {
+        return offer.itemsToGive.length === 0 && offer.itemsToReceive.length > 0;
+    },
+
+    isTF2: function(item) {
+        return item.appid == 440;
+    },
+      
+    isCraftable : function(item) {
+        var desc = item.descriptions;
+        if(!desc)
+            return false;
+
+        return desc.some((i) => {
+            return !(i.value == '( Not Usable in Crafting )');
+        });
+    }
 }
